7b98c7bf616adfa0abb85e43117e7f9c06ab5ffc
dominggo1999 <arnolddominggo19@gmail.com>
2021-02-28 11:59:52 +0700

    adding cart dropdown item

--- src/components/CartDropdown.js
+++ src/components/CartDropdown.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Button from './Button';
+import CartItem from './CartItem';
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
+  console.log(cartItems);
   return (
     <div className="cart-dropdown">
       <div className="cart-dropdown-items">
-
+        {
+          cartItems.length ? cartItems.map((item) => (
+            <CartItem
+              key={item.id}
+              item={item}
+            />
+          )) : <h1>Your cart is empty</h1>
+        }
       </div>
       <Button title="Go to checkout" />
     </div>
   );
 };
 
-export default CartDropdown;
+const mapStateToProps = (state) => ({
+  cartItems: state.cart.cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
---
+++ src/components/CartItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const CartItem = ({ item }) => {
+  console.log(item);
+  const {
+    name, imageUrl, price, quantity,
+  } = item;
+
+  return (
+    <div className="cart-item">
+      <div
+        className="cart-item-image"
+      >
+        <img
+          src={imageUrl}
+          alt="Cool Item"
+        />
+      </div>
+      <div className="cart-desc">
+        <p className="cart-item-name">{name}</p>
+        <p className="cart-item-price">{quantity} x ${price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
